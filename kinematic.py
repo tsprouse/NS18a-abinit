3f34de9444918285ab293d07c83b00835eb1b685
Patrick Fasano <patrick@patrickfasano.com>
2018-05-10 18:20:00 -0400

    kinematic: make Racah reduction more efficient

--- kinematic.py
+++ kinematic.py
@@ -33,8 +33,13 @@ def tke(n1,l1,n2,l2):
     # QMN = n l j mj tz
     # Only loop over n and l
 
-    for n in xrange(6):
-        for l in xrange(6):
+    if l1 != l2:
+        return 0.
+
+    for dn in [-1, 1]:
+        n = n2 + dn
+        for dl in [-1, 1]:
+            l = l2 + dl
             bdbd += phase(l-l1)*rme_bdag(n1,l1,n,l)*rme_bdag(n,l,n2,l2)
             bbd  += phase(l-l1)*rme_b(n1,l1,n,l)*rme_bdag(n,l,n2,l2)
             bb   += phase(l-l1)*rme_b(n1,l1,n,l)*rme_b(n,l,n2,l2)
